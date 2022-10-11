f5d01f079bfdac1fd8c18b4aaf195fb97f0f4201
vuthelong0611 <vuthelong0611@gmail.com>
2022-10-11 17:29:46 +0700

    ...

--- src/pages/DetailFood/index.js
+++ src/pages/DetailFood/index.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 const cx = classNames.bind(styles);
 function DetailFood() {
-  const {orderNow} = useGlobalContext()
+  const { orderNow } = useGlobalContext();
   const [jobs, setJobs] = useState([]);
   const fetchJobs = async () => {
     try {
@@ -26,7 +26,7 @@ function DetailFood() {
   console.log(param);
   let a;
   return (
-    <div style={{ marginTop: "100px", marginLeft: "60px" }}>
+    <div className={cx("container-big")}>
       {jobs.map((item) => {
         a = [...item.comments];
         console.log(a);
@@ -38,31 +38,45 @@ function DetailFood() {
               </div>
               <h2>{item.name}</h2>
             </div>
-            <article className={cx('art')}>
+            <article className={cx("art")}>
               <div>Grade: {item.grade}</div>
               <div>Price: {item.price}</div>
               <div>Decription: {item.description2}</div>
             </article>
-            <div className={cx('button')}>
-                  <button className={cx('btn')} onClick={() =>{orderNow(item)}}>Add to Cart</button>
-             <Link className={cx('btn1')} onClick={() =>{orderNow(item)}} to='/pay'>Order Now</Link>    
-                  </div>
+            <div className={cx("button")}>
+              <button
+                className={cx("btn")}
+                onClick={() => {
+                  orderNow(item);
+                }}
+              >
+                Add to Cart
+              </button>
+              <Link
+                className={cx("btn1")}
+                onClick={() => {
+                  orderNow(item);
+                }}
+                to="/pay"
+              >
+                Order Now
+              </Link>
+            </div>
           </div>
         );
       })}
-     <div className={cx('container1')}>
-     Comment:
-      {a &&
-        a.map((item, index) => {
-          return (
-            <div key={index} className={cx('comment')}>
-               <div>Author: {item.author}</div>
-               <div>Detail: {item.detail}</div>
-            </div>
-          );
-        })}
-     </div>
-     
+      <div className={cx("container1")}>
+        Comment:
+        {a &&
+          a.map((item, index) => {
+            return (
+              <div key={index} className={cx("comment")}>
+                <div>Author: {item.author}</div>
+                <div>Detail: {item.detail}</div>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
